f4ff3050fc5540e872b6e0032ab4d3315b645b7f
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-25 07:33:25 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'products#index'
-  post 'products/add_to_cart', to: 'products#add' as: 'add_product'
+  post 'products/add', to: 'products#add' as: 'add_product'
 end
